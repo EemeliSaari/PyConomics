ceab3f6b916c0aed9a246159703710c5f160ee6d
EemeliSaari <saari.eemeli@gmail.com>
2017-09-12 13:21:28 +0300

    DataMiner
    Simplified two functions into one.

--- sc/scrap_google.py
+++ sc/scrap_google.py
@@ -3,57 +3,45 @@ import requests
 import bs4 as bs
 
 
-def scrapper(symbol,startdate,enddate,loc='HEL',rows=200):
+def data_frame(symbol, startdate, enddate, market='HEL'):
     """
-    Scrapes Google's finance history page.
+    Converts a scrapped list from Google's Finance history 
+    page to a panda data set for further use.
 
     :param symbol: ticker of the stock company
-    :param startdate: list history from startdate
-    :param enddate: list history to enddate
-    :param loc: markets ticker
-    :param rows: the amount of rows to be pulled at the same time
-
-    :return: list rows containing [date,open,high,low,close,volume]
+    :param startdate: start date of the data frame
+    :param enddate: end date for the data frame
+    :param market: market that we're using available:HEL (default)
+                                                     ---
+    :return: .csv file of the given stock symbol in the following format: 
+                                        header=date;open;high;low;close;volume
     """
     i = 0
+    rows = 200
     data = []
-    state = True
-    while state:
+    loop = True
+    while loop:
         try:
-            url = 'https://www.google.com/finance/historical?q={:s}%3A{:s}&startdate={:s}&enddate={:s}&start={:d}&num={:d}'.format(loc,symbol,startdate,enddate,i,rows)
+            url = 'https://www.google.com/finance/historical?q={:s}%3A{:s}&startdate={:s}&enddate={:s}&start={:d}&num={:d}'.format(market, symbol, startdate, enddate, i, rows)
             response = requests.get(url)
             soup = bs.BeautifulSoup(response.text,'lxml')
             
             table = soup.find('table',{'class':'gf-table historical_price'})
             
             for head in table.findAll('tr')[1:]:
-                date = [head.find('td',{'class':'lm'}).text.strip()]
+                date = [head.find('td', {'class':'lm'}).text.strip()]
 
                 rest = []
-                for p in head.findAll('td',{'class':'rgt'}): 
+                for p in head.findAll('td', {'class':'rgt'}): 
                     rest.append(p.text.strip())
                 
-                data.append(date+rest)
+                data.append(date + rest)
 
             i += 200
 
         except AttributeError:
-            state = False
-            return data
+            loop = False
+            
+    header = ['date', 'open', 'high', 'low', 'close', 'volume']
 
-
-def data_frame(symbol,startdate,enddate):
-    """
-    Converts a scrapped list to a panda data set for further use.
-
-    :param symbol: ticker of the stock company
-    :param startdate: start date of the data frame
-    :param enddate: end date for the data frame
-    
-    :return: .csv file of of the given stock symbol with 
-                    header=date;open;high;low;close;volume
-    """
-    header = ['date','open','high','low','close','volume']
-    data = scrapper(symbol,startdate,enddate)
-
-    return pd.DataFrame(data=data,columns=header)
+    return pd.DataFrame(data=data, columns=header)
