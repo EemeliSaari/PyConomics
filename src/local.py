6592757573fd453feef37f2a595263b65d850f51
EemeliSaari <saari.eemeli@gmail.com>
2018-01-17 21:23:08 +0200

    Reworked scripts
    Reworked most of the scripts

--- src/local.py
+++ src/local.py
@@ -1,48 +1,72 @@
-# Saves the mined data into a .pickle dumps, compiles and converts them
-# for further use.
+# Saves the mined data into csv, pickle, sqlite
 
 import os
 import pickle
 
 import pandas as pd
 
-from web_scraper import get_omx
-from scrap_google import data_frame
-from utils import convert_date, convert_volume_format
+from src.web_scraper import get_omx_data, get_price_df
+from src.utils import convert_date, convert_volume_format
 
-def get_symbols(location):
+
+def symbols_to_pickle(symbols, market, path):
     """
     Gets symbol list and saves it to pickle dump
 
-    :param location: market location, helsinki, iceland, stockholm etc.
-    :param dpath: directory path that pickle dump is saved into.
+    :param market: market location, helsinki, iceland, stockholm etc.
+    :param path: directory path that pickle dump is saved into.
     """
     # target name for saved file.
-    target = 'OMX_{:s}_symbols.pickle'.format(location)
+    target = 'OMX_{:s}_symbols.pickle'.format(market)
     # directory path that list of symbols is saved into.
-    symbols_dir = './.local/symbols_list/'
+    symbols_dir = '{:s}/symbols_pickles/'.format(path)
 
     if not os.path.exists(symbols_dir):
         os.mkdir(symbols_dir)
 
-    if not os.path.exists(symbols_dir + target):
-        with open((symbols_dir + target), 'wb') as f:
-            data = get_omx(location)
+    target = symbols_dir + target
+    if not os.path.exists(target):
+        with open(target, 'wb') as f:
+            pickle.dump(symbols, f)
+    else:
+        with open(target, 'rb') as f:
+            symbols_old = pickle.load(f)
+
+        if symbols != symbols_old:
+            with open(target, 'wb') as f:
+                pickle.dump(symbols, f)
+
+def markets_to_pickle(market, data, path):
+    """
+    Saves list of markets into pickles dump
+
+    :param markets:
+    :param data:
+    :param path:
+    """
+    directory = '{:s}/markets_list/'.format(path)
+
+    target = directory + '{:s}_markets_list.pickle'.format(market)
+
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+
+    if not os.path.exists(target):
+        with open(target, 'wb') as f:
             pickle.dump(data, f)
 
 
-def get_df(location, startdate, enddate, target_path=False):
+def df_to_pickle(market, startdate, enddate, *args, target_path=False):
     """
     Saves the panda dataframe acquired from Googles Finance history search
     into a pickle dump for local usage so we don't have to do heavy processing
     power required work everytime.
 
-    :param location: markets location, helsinki, iceland, stockholm etc.
+    :param market: markets location, helsinki, iceland, stockholm etc.
     :param target_path: saving location, default: './.local/df_stock_pickle/'
     :param startdate: list history from startdate
     :param enddate: list history to enddate
     """
-
     # path for symbols
     symbol_path = './.local/symbols_list/'
     if not target_path:
@@ -51,19 +75,18 @@ def get_df(location, startdate, enddate, target_path=False):
 
         if not os.path.exists('./.local/df_stock_pickle/'):
             os.mkdir('./.local/df_stock_pickle/')
-    
-    # TODO: Add all of the markets
-    m = {'helsinki':'HEL'}
+
+    MARKET = {'helsinki':'HEL'}
     # naming convention for pickle dump
-    symbols = 'OMX_{:s}_symbols.pickle'.format(location)
-    
+    symbols = 'OMX_{:s}_symbols.pickle'.format(market)
+
     with open((symbol_path + symbols),'rb') as symbol_file:
         symbols_list = pickle.load(symbol_file)
-    
+
     for symbol in symbols_list: 
 
         if not os.path.exists(target_path+symbol+'.pickle'):
-            df = data_frame(symbol, startdate, enddate, market=m[location])
+            df = get_price_df(symbol, startdate, enddate, market=MARKET[location])
 
             with open(target_path+symbol+'.pickle','wb') as f:
                 pickle.dump(df,f)
@@ -71,53 +94,49 @@ def get_df(location, startdate, enddate, target_path=False):
             print('Already have it')
 
 
-def compile_data(market, column, target_path=False):
+def compile_data(market, column, symbols_path, df_path, target_path=False):
     """
     Compiles data from all of the pickle dumps to one .csv file.
 
     :param market: to specify saved file
     :param column: type we're joining [date,open,high,low,close,volume]
+    :param symbols_path: path for symbols
+    :param df_path: path for stock data frames
     :param target_path: path to save compiled data into, default: './.local/df_compiled/'
     """
-    # path for symbols
-    symbols_path = './.local/symbols_list/'
-    # path for stock data frames
-    df_path = './.local/df_stock_pickle/'
-
     symbols_file = symbols_path + 'OMX_{:s}_symbols.pickle'.format(market)
 
     with open(symbols_file,'rb') as f:
         symbols_list = pickle.load(f)
-
-    if not os.path.exists('./.local/df_compiled'):
-        os.mkdir('./.local/df_compiled')
 
     new_df = pd.DataFrame()
 
     for symbol in symbols_list:
         with open(df_path + symbol + '.pickle', 'rb') as f:
             df = pickle.load(f)
-        
+
         df['date'] = df['date'].apply(convert_date)
-        df.set_index('date',inplace=True)
-        
+        df.set_index('date', inplace=True)
+
+        #TODO replace the drop to handle the column param
         df.drop(['open','high','low','volume'], 1, inplace=True)
         df.rename(columns = {column: symbol}, inplace=True)
-        
+
         if new_df.empty:
             new_df = df
         else:
-            new_df =  new_df.join(df,how='outer')
-        
+            new_df =  new_df.join(df, how='outer')
+
     # we can select to save the file into specific directory
     if not target_path:
-        new_df.to_csv('./.local/df_compiled/OMX_{:s}_joined_{:s}.csv'.format(market,column))
+        return new_df
     else:
+        if not os.path.exists('./.local/df_compiled'):
+            os.mkdir('./.local/df_compiled')
         new_df.to_csv(target_path + 'OMX_{:s}_joined_{:s}.csv'.format(market,column))
 
 
-def pickle_to_csv(pickles_path = './.local/df_stock_pickle/',
-                  new_path = './.local/df_stock_csv/'):
+def pickle_to_csv(pickles_path, new_path):
     """
     Converts the aquired pickle dumps to .csv files
 
@@ -131,7 +150,7 @@ def pickle_to_csv(pickles_path = './.local/df_stock_pickle/',
     # keeping track of handled files
     handled = []
     for file_name in os.listdir(pickles_path):
-        
+
         if file_name not in handled:
             with open(pickles_path + file_name, 'rb') as f:
 
@@ -141,7 +160,7 @@ def pickle_to_csv(pickles_path = './.local/df_stock_pickle/',
                 df['date'] = df['date'].apply(convert_date)
                 df['volume'] = df['volume'].apply(convert_volume_format)
                 df.set_index('date',inplace=True)
-                
+
                 csv = df.to_csv(new_path + "{:}.csv".format(file_name.strip('.pickle')))
 
                 handled.append(file_name)
