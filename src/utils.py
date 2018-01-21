67169d63f71c4c776036595a4fab50677faf48c2
EemeliSaari <saari.eemeli@gmail.com>
2018-01-21 15:32:11 +0200

    Minor changes, comments
    Minor changes to the utilities, added comments

--- src/utils.py
+++ src/utils.py
@@ -4,19 +4,27 @@ from datetime import date
 import time
 
 
-def name_stock_ticker(ticker):
-
-    parts = ticker.split(" ")
+def name_stock_symbol(symbol, sep='-'):
+    """
+    Function takes stock symbol, separates spaces and replaces with separator
+    """
+    parts = symbol.split(" ")
     if len(parts) > 1:
-        ticker = "-".join(parts)
-    else:
-        ticker = ticker
+        symbol = sep.join(parts)
 
-    return ticker
+    return symbol
+
+def parse_null(str_list):
+    """
+    Parse string list for null entries
+    """
+    return [row for row in str_list if 'null' not in row.split(',')[0:len(row.split(','))]]
 
 
 def datestring_to_int(string_date):
-
+    """
+    Converts datestring to int
+    """
     return int(time.mktime(dt.strptime(string_date, '%d.%m.%Y').timetuple()))
 
 def convert_date(date):
@@ -27,15 +35,22 @@ def convert_date(date):
     return dt.strptime(date,'%Y-%m-%d').strftime('%d.%m.%Y')
 
 def get_today_datetime():
-
+    """
+    Function returns today as a datetime object
+    """
     return dt.combine(date.today(), dt.min.time())
 
 def string_to_date(string_date):
-
+    """
+    Convert string to datetime object
+    """
     return dt.strptime(string_date, '%d.%m.%Y')
 
-def convert_volume_format(string):
+def convert_volume(string):
     """
     Converts volume
     """
-    return int(string.replace(',','')) / 1000000
+    if string != 'null':
+        return int(string.replace(',',''))
+    else:
+        return 0
