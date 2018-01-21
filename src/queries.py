dc7e6b7aa6083f5e13456507e28fa14a4a838574
EemeliSaari <saari.eemeli@gmail.com>
2018-01-21 15:32:38 +0200

    Reworked the class

--- src/queries.py
+++ src/queries.py
@@ -1,10 +1,12 @@
+import codecs
+import json
 import re
 from time import time
 
 import bs4 as bs
 import requests as req
 
-from src.utils import datestring_to_int
+from src.utils import datestring_to_int, name_stock_symbol
 
 
 class YahooQuery:
@@ -12,53 +14,94 @@ class YahooQuery:
     def __init__(self, symbol, market):
 
         self.__MARKETS = {
-            "Helsinki" : "HE",
-            "Stockholm" : "ST",
-            "Baltic" : "TL",
-            "Copenhagen" : "CO",
-            "Iceland" : "L"
-        } # TODO add rest of the markets
+            "Helsinki" : ["HE"],
+            "Stockholm" : ["ST"],
+            "Baltic" : ["TL", 'RG', 'VS'],
+            "Copenhagen" : ["CO"],
+            "Iceland" : ["L", "IC"]
+        }
 
-        self.__symbol = symbol
+        self.__symbol = name_stock_symbol(symbol)
         self.__market = market
 
-    def get_crumb(self, base):
+    def get_crumb(self):
+        """
+        Parses the http request for crumb and cookies
 
-        res = req.get(base)
-        cookies = res.cookies['B']
-        # Fetch the crump from cookies with regex
-        pattern = re.compile('.*"CrumbStore":\{"crumb":"(?P<crumb>[^"]+)"\}')
-        return [pattern.match(x).groupdict()['crumb'] for x in res.text.splitlines() if pattern.match(x)][0], cookies
+        :return: crumb, cookies, working stock symbol
+        """
+        for padding in self.__MARKETS[self.__market]:
+            
+            symbol = ".".join([self.__symbol, padding])
+            URL = "https://finance.yahoo.com/quote/{:s}/history".format(symbol)
+            try:
+                res = req.get(URL)
+                cookies = res.cookies['B']
+                # Fetch the crump from cookies with regex
+                pattern = re.compile('.*"user":\{"crumb":"(?P<crumb>[^"]+)"\,')
+                crumb = self.parse_crumb([pattern.match(x).groupdict()['crumb'] for x in res.text.splitlines() if pattern.match(x)][0])
+                return crumb, cookies, symbol
 
-    def get_historic(self, start, end, interval):
+            except KeyError:
+                pass
 
-        symbol = ".".join([self.__symbol, self.__MARKETS[self.__market]])
+    def parse_crumb(self, raw_crumb):
+        """
+        Decode the raw_crumb
+        """
+        return str(codecs.decode(raw_crumb, 'unicode-escape'))
 
-        URL = 'https://query1.finance.yahoo.com/v7/finance/download/{:s}'.format(self.__symbol)
+    def get_historic(self, end, interval, start=None):
+        """
+        Yahoo query for historic stock price
 
-        crumb, cookies = self.get_crumb("https://finance.yahoo.com/quote/{:s}/history".format(symbol))
-        if start:
-            start = datestring_to_int(start)
-        else:
-            start = 0
+        :param start: starting date as string format('%d.%m.%Y'),
+                      default(None), will return the max
+        :param end: end date as string format('%d.%m.%Y')
+        :param interval: interval for search, available: [1d, 1wk, 1mo]
+        """
+        try:
+            URL = 'https://query1.finance.yahoo.com/v7/finance/download/{:s}'.format(self.__symbol)
 
-        params = {
-            'symbol' : symbol,
-            'period1' : start,
-            'period2' : datestring_to_int(end),
-            'interval' : interval,
-            'crumb' : crumb
-        }
-        cookies = {
-            'B' : cookies
-        }
+            crumb, cookies, symbol = self.get_crumb()
 
-        resp = req.get(URL, params=params, cookies=cookies)
-        self.parse_errors(resp)
+            if start:
+                start = datestring_to_int(start)
+            else:
+                start = 0
 
-        return resp.text
+            params = {
+                'symbol' : symbol,
+                'period1' : start,
+                'period2' : datestring_to_int(end),
+                'interval' : interval,
+                'crumb' : crumb
+            }
+            cookies = {
+                'B' : cookies
+            }
 
-    def parse_errors(response):
-        #TODO check for errors
-        print(response.splitlines()[0])
-        pass
+            resp = req.get(URL, params=params, cookies=cookies)
+            if self.check_errors(resp.text):
+                return resp.text
+
+        except TypeError:
+            pass
+
+        except req.exceptions.ContentDecodingError:
+            pass
+        
+        return None
+
+    def check_errors(self, response):
+        """
+        Check the response for any errors
+        """
+        # Errors are represented in JSON fromat
+        try:
+            js = json.loads(response)
+            print(js)
+            return False
+        # In case of failure we know that there was no error
+        except json.decoder.JSONDecodeError:
+            return True
