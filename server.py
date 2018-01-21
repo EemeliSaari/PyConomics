0e2eb5d6a47a29ec6899f6e636042a269e3fc7a7
EemeliSaari <saari.eemeli@gmail.com>
2018-01-21 16:57:12 +0200

    Added comments

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import os
 import sys
 from functools import partial
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, jsonify, render_template, request
 
 ROUTES = dict()
 def route(route=None):
@@ -14,15 +14,21 @@ def route(route=None):
 
 
 class Server(Flask):
-
+    """Simple backend server inherited from Flask"""
 
     def __init__(self, database, *args, **kwargs):
+        """
+        Constructor for the class
 
+        :param database: DataBase object
+        """
         if not args:
             kwargs.setdefault('import_name', __name__)
 
+        # Init the Flask app
         Flask.__init__(self, *args, **kwargs)
 
+        # Map all the routes
         for route, func in ROUTES.items():
             part_func = partial(func, self)
             part_func.__name__ = func.__name__
@@ -32,9 +38,10 @@ class Server(Flask):
 
     @route('/')
     def index(self):
+        """Renders the html page"""
         return render_template('index.html')
     
     @route('/data/<string:symbol>')
     def get_stock(self, symbol):
-
+        """Returns symbols price date in json format"""
         return jsonify(self.__database.get_stock(symbol))
