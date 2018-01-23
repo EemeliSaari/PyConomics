555ddf805134f1828a9a00326e09ca82b432374f
EemeliSaari <saari.eemeli@gmail.com>
2018-01-23 17:24:19 +0200

    Replaced optparse with argparse

--- pyconomist.py
+++ pyconomist.py
@@ -1,4 +1,4 @@
-from optparse import OptionParser
+import argparse
 
 from database import DataBase
 from src.routines import check_date_routine
@@ -6,23 +6,16 @@ from src.routines import check_date_routine
 from server import Server
 
 def main():
-    """
-    Main routine for software
-    """
-    # Check the state of the database
-    db = DataBase()
+    """Main routine for software"""
+
     # Everytime the program is started, we check if the data is up to date
-    #check_date_routine()
-
-    parser = OptionParser()
-    parser.add_option("-p", "--port", dest="port")
-    (options, args) = parser.parse_args()
-
-    #TODO add more options
+    check_date_routine()
 
     # Start the server
-    Server(database=db, template_folder='templates').run(port=3000, debug=True)
-
+    Server(database=DataBase(), template_folder='templates').run(port=3000, debug=True)
 
 if __name__ == '__main__':
+
+    #TODO add more options
+    parser = argparse.ArgumentParser()
     main()
